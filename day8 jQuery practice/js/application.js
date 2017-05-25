4b00eb0b740c32d056853fbbb3fa62e3fd6af933
Alex Min <m98stone@gmail.com>
2017-05-25 13:51:31 +0800

    add some bootstrap

--- day8 jQuery practice/js/application.js
+++ day8 jQuery practice/js/application.js
@@ -1,81 +1,121 @@
 $(document).ready(function () {
 
+// Create Item //
   var createItem = function () {
+
     var itemName = $('#addItemNameValue').val();
+    //var itemName = $("input[name='name']").val();
     var itemPrice = $('#addItemPriceValue').val();
     itemPrice = Number(itemPrice).toFixed(2);
 
+    // valide input name check
     if (itemName === '') {
       return alert('Please enter item name.')
     }
-    if (Number.isNaN(Number(itemPrice)) === true) {
-      $('#addItemNameValue').val('');
-      $('#addItemPriceValue').val('');
-      return alert('Please enter number.');
-    } else if (Number.isNaN(Number(itemPrice)) !== true) {
 
-      var emptyList = '<div class="row listedItem">\
-                      <div class="itemName col-xs-4">' +
-        itemName + '</div>\
-                      <div class="unitPrice col-xs-3">' +
-        '$' + itemPrice + '</div>\
-                      <div class="itemQty col-xs-3">\
-                        <label for="qty">QTY</label>\
-                          <input type="text" class="qty" value="0">\
-                        <button type="button" class="removeItem">Cancel</button>\
+    // html code of item
+    var emptyList = '<div class="row listedItem">\
+                      <div class="itemName col-xs-3 col-md-4">' + itemName + '</div>\
+                      <div class="unitPrice col-xs-2 col-md-3">$' + itemPrice + '</div>\
+                      <div class="itemQty col-xs-3 col-md-3">\
+                        <div class="input-group">\
+                        <span class="input-group-addon">QTY</span>\
+                          <input type="number" min="0" class="qty form-control" value="0"/>\
+                        </div>\
                       </div>\
-                      <div class="subTotal col-xs-2">' + '$0.00' + '</div>\
+                      <div class="subTotal col-xs-2 col-md-1">$0.00\
+                      </div>\
+                      <i class="removeItem fa fa-trash fa-2x col-xs-2 col-md-1" aria-hidden="true"></i>\
                     </div>';
 
-      $('#addItemNameValue').val('');
-      $('#addItemPriceValue').val('');
+    // reset current input
+    $('#addItemNameValue').val('');
+    $('#addItemPriceValue').val('');
 
-      $('#itemList').prepend(emptyList);
-    }
-    
-    $('.removeItem').on('click', removeItem);
-    $('.qty').on('keyup', updatingSubSum);
-    $('.removeItem').on('click', removeItem);
+    // append the item
+    $(emptyList).prependTo($('#itemList')).hide().slideDown(200);
+    //$('#itemList').prepend(emptyList).hide().slideDown(400);
+
+    // attach eventlistener to the new item
+    init();
   };
 
+// update sub total of each item.
   var updatingSubSum = function (e) {
     var $target = $(e.target || e.srcElement);
     var $item = $target.parents('.listedItem');
     var $subTotal = $item.find('.subTotal');
 
     var quantity = Number($target.val());
+
+    // positive quantity checker
+    if (quantity < 0) {
+      $target.val(0);
+      return alert('Not a positive number.');
+    }
+
+    // trim/clean the zero of init value
+    $target.val(Math.floor(quantity));
+
+    // display the result
     var unitPrice = Number($item.find('.unitPrice').text().trim().substr(1));
     $subTotal.text('$' + (quantity * unitPrice).toFixed(2));
 
     updatingTotalSum();
   };
 
-
   var updatingTotalSum = function () {
     var totalSum = 0;
     $('.subTotal').each(function (i, el) {
       totalSum += Number($(el).text().trim().substr(1));
     });
+    // display the result
     $('#totalSum').text('$' + totalSum.toFixed(2));
   };
 
   var removeItem = function (e) {
-    var itemRow = $(e.target || e.srcElement).parents('.listedItem').remove()
-
-    /*
-    var message = '<div class="blue">Are you sure for removing the item?\
-                   </div>';
-    itemRow.append(message).delay(1500)
-    //.queue(function() { $(this).remove(); });
-    */
-    updatingTotalSum();
+    $(e.target).parents('.listedItem').delay(300).queue(function () {
+      //remove the item with 500ms delay
+      $(this).remove();
+      updatingTotalSum();
+    });
   };
 
-  (function () {
-    $('#createItemBtn').on('click', createItem);
+  //event listeners
+  var init = function () {
     $('.qty').on('keyup', updatingSubSum);
+    $('.removeItem').on('click',removeItem);
+    $('.removeItem').hover(function () {
+      $(this).css('color', 'rgb(232, 78, 78)')
+    }, function () {
+      $(this).css('color', 'black')
+    });
+  };
+
+  // attach event listeners.
+  (function () {
+    init();
+    $('#createItemBtn').on('click', createItem);
     $('#calButton').on('click', updatingTotalSum);
-    $('.removeItem').on('click', removeItem);
   }());
+
+
+
+/* delegation pattern
+
+  $('#cart').on('keyup', '*', function(event){
+    var productRow = $(event.target).parents('.product')[0];
+    updateSubTotal(productRow);
+  });
+
+  $('#cart').on('keyup', '.delete', function(event){
+    var productRow = $(event.target).parents('.product')[0];
+    deleteProduct(productRow);
+  });
+
+  $('.create').on('click','.createProduct', function(event){
+    createProduct();
+  });
+*/
 
 });
